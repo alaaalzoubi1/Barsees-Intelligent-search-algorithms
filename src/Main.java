501ad6d10a1d2f60bd06550eca4d7ad709343d15
Ameer <ameermatr@yahoo.com>
2024-01-07 13:49:11 +0300

    The boss changes

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,75 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-      Board board = new Board(12,4);
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in); // Create a Scanner object
+    Board board = new Board(12,4);
+//
+//      DiceRolls rand = new DiceRolls();
+//      rand.rollDice();
+//      rand.printState();
+    Player player1 = new Player("EMPLOYEE",board,1);
+    Player player2 = new Player("THE BOSS",board,2);
+    PlayRock[] x = player1.getPlayRocks();
+    PlayRock[] y = player2.getPlayRocks();
+    board.setPieceInPlayerKitchen(0,x[1]);
+    board.setPieceInPlayerKitchen(0,y[2]);
+    board.printBoard();
+    System.out.println(board.getPath().length);
+    while(true){
+      System.out.println("Press 1 to roll the dice or 0 to exit");
+      int input = scanner.nextInt();// Read user input
+      if (input == 0) {
+        break; // Exit the loop if 0 is entered
+      }
+      else if (input == 1) {
+        DiceRolls rand = new DiceRolls();
+        rand.rollDice();
+        rand.printState(); // This will show the dice result
 
-      DiceRolls rand = new DiceRolls();
-      rand.rollDice();
-      rand.printState();
-      Player player1 = new Player("alaa",board,1);
-      Player player2 = new Player("aamir",board,2);
-      PlayRock[] x = player1.getPlayRocks();
-      PlayRock[] y = player2.getPlayRocks();
-      board.setPieceInPlayerKitchen(0,x[1]);
-      board.setPieceInPlayerKitchen(0,y[2]);
-      board.printBoard();
-      System.out.println(board.getPath().length);
-      for (int i = 0; i < 10; i++) {
-        try {
-          Move.DoMove(player1.getPlayRocks()[1],board );
-          board.printBoard();
-//          System.out.println("Iteration: " + i);
-//          System.out.println("counter : " + player1.getPlayRocks()[1].counter);
-//          System.out.println("ts : " + player1.getPlayRocks()[1].tastee7);
-//          System.out.println("inth : " + player1.getPlayRocks()[1].isInTheKitchen);
-          Thread.sleep(1000); // Sleep for 5 seconds
-        } catch (InterruptedException e) {
-          e.printStackTrace();
+        // Assuming you want to move for player1, display available rocks
+        PlayRock[] availableRocks = player2.getPlayRocks();
+        System.out.println("Available rocks for " + player1.getName() + ":");
+        for (int i = 0; i < availableRocks.length; i++) {
+          System.out.println("Rock " + (i + 1) + ": Position " + availableRocks[i].getPosition());
         }
+
+        // Ask the user to choose a rock to move
+        System.out.println("Choose a rock number to move:");
+        int rockNumber = scanner.nextInt();
+        if (rockNumber >= 1 && rockNumber <= availableRocks.length) {
+
+          String diceResult = rand.countOnesAndNameState();
+          Move.DoMove(availableRocks[rockNumber - 1], board, diceResult);
+
+          board.printBoard(); // Print the updated board state
+        } else {
+          System.out.println("Invalid rock number!");
+        }
+      } else {
+        System.out.println("Invalid input!");
       }
 
-
-
-
-        }
     }
+    scanner.close(); // Close the scanner when it's no longer needed
+    System.out.println("Game over. Thank you for playing!");
+
+//    }
+//      for (int i = 0; i < 10; i++) {
+//        try {
+//          Move.DoMove(player2.getPlayRocks()[1],board );
+//          board.printBoard();
+//          Thread.sleep(1000); // Sleep for 5 seconds
+//        } catch (InterruptedException e) {
+//          e.printStackTrace();
+//        }
+//      }
+//
+//
+//
+//
+//        }
+  }
+}
--- src/Move.java
+++ src/Move.java
@@ -1,32 +1,51 @@
+import java.util.Scanner;
+
 public class Move {
 //    Board board;
 //    PlayRock playRock;
 
-//    public Move(Board board, PlayRock playRock) {
+    //    public Move(Board board, PlayRock playRock) {
 //        this.board = board;
 //        this.playRock = playRock;
 //    }
-    public static void DoMove(PlayRock playRock, Board board)
+    public static void DoMove(PlayRock playRock, Board board,String diceResult)
     {
-        String x = "Duwag";
         DiceRolls diceRolls = new DiceRolls();
-        diceRolls.countOnesAndNameState();
-        switch (x)
+        String x =  diceRolls.countOnesAndNameState();
+
+        if (x.equals("Dest") || x.equals("Bunja")) {
+            System.out.println("You rolled a " + diceResult + "! Choose an option:");
+            System.out.println("1. Add a new rock to the board");
+            System.out.println("2. Move an available rock 1 position");
+
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+
+            if (choice == 1) {
+                // Logic to add a new rock to the board
+                // This could involve checking if there is a rock available to enter the board
+                // and updating the board state accordingly
+            } else if (choice == 2) {
+                // Move the passed playRock 1 position
+                if (playRock.isInTheKitchen) {
+                    board.movePlayRockInKitchen(playRock, 1, board.getPlayerKitchen(playRock));
+                } else {
+                    board.movePlayRockInPath(playRock, 1, board.getPath());
+                }
+            } else {
+                System.out.println("Invalid choice!");
+            }
+        }
+        else{ switch (x)
         {
             case "dest" :
-//                if (playRock.getPosition() == 0) {
-//                    playRock.setPosition(playRock.getPosition() + 10);
-//                } else {
-//                    playRock.setPosition(playRock.getPosition() + 10);
-//                    if (playRock.getPosition() >= board.getPath().length) {
-//                        playRock.setPosition(0);
-//                        if (playRock.getPlayer().getName().equals("Player 1")) {
-//                            board.setPieceInPlayer1Kitchen(0, playRock);
-//                        } else {
-//                            board.setPieceInPlayer2Kitchen(0, playRock);
-//                        }
-//                    }
-//                }
+                if(playRock.isInTheKitchen)
+                {
+                    board.movePlayRockInKitchen(playRock,10, board.getPlayerKitchen(playRock));
+                }
+                else {
+                    board.movePlayRockInPath(playRock,10, board.getPath());
+                }
             case "Duwag" :
                 if(playRock.isInTheKitchen)
                 {
@@ -37,20 +56,56 @@ public class Move {
                 }
                 break;
             case  "Thalatha" :
-                playRock.setPosition(playRock.getPosition() + 3);
+                if(playRock.isInTheKitchen)
+                {
+                    board.movePlayRockInKitchen(playRock,3, board.getPlayerKitchen(playRock));
+                }
+                else {
+                    board.movePlayRockInPath(playRock,3, board.getPath());
+                }
+                //playRock.setPosition(playRock.getPosition() + 3);
                 break;
             case  "Arba'a" :
-                playRock.setPosition(playRock.getPosition() + 4);
+                if(playRock.isInTheKitchen)
+                {
+                    board.movePlayRockInKitchen(playRock,4, board.getPlayerKitchen(playRock));
+                }
+                else {
+                    board.movePlayRockInPath(playRock,4, board.getPath());
+                }
+                //playRock.setPosition(playRock.getPosition() + 4);
                 break;
             case  "Bunja" :
-                playRock.setPosition(playRock.getPosition() + 24);
+                if(playRock.isInTheKitchen)
+                {
+                    board.movePlayRockInKitchen(playRock,24, board.getPlayerKitchen(playRock));
+                }
+                else {
+                    board.movePlayRockInPath(playRock,24, board.getPath());
+                }
+                //playRock.setPosition(playRock.getPosition() + 24);
                 break;
             case  "Bara" :
-                playRock.setPosition(playRock.getPosition() + 12);
+                if(playRock.isInTheKitchen)
+                {
+                    board.movePlayRockInKitchen(playRock,12, board.getPlayerKitchen(playRock));
+                }
+                else {
+                    board.movePlayRockInPath(playRock,12, board.getPath());
+                }
+                //playRock.setPosition(playRock.getPosition() + 12);
                 break;
-            case  "Shakka" :
-                playRock.setPosition(playRock.getPosition() + 6);
+            case  "Shakka" :if(playRock.isInTheKitchen)
+            {
+                board.movePlayRockInKitchen(playRock,6, board.getPlayerKitchen(playRock));
+            }
+            else {
+                board.movePlayRockInPath(playRock,6, board.getPath());
+            }
+                //   playRock.setPosition(playRock.getPosition() + 6);
                 break;
-        }
+        }}
+
     }
+
 }
